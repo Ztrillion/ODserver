c18712448431fff4e348055fb4719eda117d6549
Ztrillion <85321962+Ztrillion@users.noreply.github.com>
2023-10-25 16:47:58 +0900

    Add files via upload

--- api_server.py
+++ api_server.py
@@ -7,18 +7,18 @@ app = FastAPI()
 
 gen_model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/general_model.pt', force_reload=True)
 gen_model.eval()
-custom_model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/custom_model.pt', force_reload=True)
-custom_model.eval()
+# custom_model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/custom_model.pt', force_reload=True)
+# custom_model.eval()
 @app.post("/predict/")
 async def predict(file: UploadFile = File(...)):
     image_data = await file.read()
     image = Image.open(io.BytesIO(image_data))
     
-    gen_results = gen_model(image)
-    gen_xyxy = gen_results.pandas().xyxy[0].to_dict(orient="records")
-    gen_xywhn = gen_results.pandas().xywhn[0].to_dict(orient="records")
+    results = gen_model(image)
+    xyxy = results.pandas().xyxy[0].to_dict(orient="records")
+    xywhn = results.pandas().xywhn[0].to_dict(orient="records")
 
-    cus_results = custom_model(image)
-    cus_xyxy = cus_results.pandas().xyxy[0].to_dict(orient="records")
-    cus_xywhn = cus_results.pandas().xywhn[0].to_dict(orient="records")
-    return {"gen_xyxy" : gen_xyxy, "gen_xywhn" : gen_xywhn, "cus_xyxy" : cus_xyxy, "cus_xywhn" : cus_xywhn}
+    # cus_results = custom_model(image)
+    # cus_xyxy = cus_results.pandas().xyxy[0].to_dict(orient="records")
+    # cus_xywhn = cus_results.pandas().xywhn[0].to_dict(orient="records")
+    return {"gen_xyxy" : xyxy, "gen_xywhn" : xywhn, "cus_xyxy" : xyxy, "cus_xywhn" : xywhn}
