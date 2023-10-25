36abbd16b984a5d598bf4f057b7f46b9381029c5
Ztrillion <85321962+Ztrillion@users.noreply.github.com>
2023-10-25 17:41:44 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ import io
 
 app = FastAPI()
 
-dt_model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/general_model.pt', force_reload=True)
-dt_model.eval()
+gen_model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/general_model.pt', force_reload=True)
+gen_model.eval()
 
 @app.post("/predict/")
 async def predict(file: UploadFile = File(...)):
+    
     image_data = await file.read()
     image = Image.open(io.BytesIO(image_data))
     
-    results = dt_model(image)
+    results = gen_model(image)
     xyxy = results.pandas().xyxy[0].to_dict(orient="records")
     xywhn = results.pandas().xywhn[0].to_dict(orient="records")
-    return {"xyxy" : xyxy, "xywhn" : xywhn}
+
+    return {"xyxy" : xyxy, "xywhn" : xywhn, }
