d551d4e12dce0af7bfdcbc2636c0fe7b59806b44
Ztrillion <85321962+Ztrillion@users.noreply.github.com>
2023-10-25 17:03:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,20 +5,15 @@ import io
 
 app = FastAPI()
 
-gen_model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/general_model.pt', force_reload=True)
-gen_model.eval()
-# custom_model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/custom_model.pt', force_reload=True)
-# custom_model.eval()
+dt_model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/general_model.pt', force_reload=True)
+dt_model.eval()
+
 @app.post("/predict/")
 async def predict(file: UploadFile = File(...)):
     image_data = await file.read()
     image = Image.open(io.BytesIO(image_data))
     
-    results = gen_model(image)
+    results = dt_model(image)
     xyxy = results.pandas().xyxy[0].to_dict(orient="records")
     xywhn = results.pandas().xywhn[0].to_dict(orient="records")
-
-    # cus_results = custom_model(image)
-    # cus_xyxy = cus_results.pandas().xyxy[0].to_dict(orient="records")
-    # cus_xywhn = cus_results.pandas().xywhn[0].to_dict(orient="records")
-    return {"gen_xyxy" : xyxy, "gen_xywhn" : xywhn, "cus_xyxy" : xyxy, "cus_xywhn" : xywhn}
+    return {"xyxy" : xyxy, "xywhn" : xywhn}
